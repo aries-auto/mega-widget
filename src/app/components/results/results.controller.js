3831e10b1c73d7c783a902287ee4724fed1b75f1
stinkyfingers <john_shenk@hotmail.com>
2015-09-24 16:57:48 -0500

    adds upc to results

--- src/app/components/results/results.controller.js
+++ src/app/components/results/results.controller.js
@@ -1,7 +1,17 @@
 class ResultsController {
-  constructor () {
+  constructor ($scope, $rootScope) {
     'ngInject';
 
+    $scope.parts = $rootScope.parts;
+
+    for (let i = 0; i < $scope.parts.length; i++){
+	    for (let j = 0; j < $scope.parts[i].attributes.length; j++){
+	    	if($scope.parts[i].attributes[j].key.toLowerCase() === 'upc'){
+	    		$scope.parts[i].upc = $scope.parts[i].attributes[j].value;
+	    		break;
+	    	}
+	    }
+    }
     
   }
 }
