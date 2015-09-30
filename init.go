e6ce797d2ada678876f1c80741ad18c364a57d19
Alex Ninneman <alex@ninneman.org>
2015-09-30 08:03:11 -0500

    init appengine testing ground

---
+++ init.go
@@ -0,0 +1,26 @@
+package widget
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+var (
+	r = render.New(render.Options{
+		Directory:     "dist",            // Specify what path to load the templates from.
+		Extensions:    []string{".html"}, // Specify extensions to load for templates.
+		IsDevelopment: true,              // Render will now recompile the templates on every HTML response.
+	})
+)
+
+func init() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		r.HTML(w, 200, "index", nil)
+	})
+
+	// Handle all requests using net/http
+	http.Handle("/", mux)
+}
